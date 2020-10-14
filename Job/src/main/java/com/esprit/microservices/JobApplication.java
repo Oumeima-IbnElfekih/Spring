9712077c6c23df8df073e7edde2c5d63e334341f
Oumeima IBNELFEKIH <oumeima.ibnelfekih@esprit.tn>
2020-10-14 11:32:49 +0100

    Add eureka client config to job and candidat MS

--- Job/src/main/java/com/esprit/microservices/JobApplication.java
+++ Job/src/main/java/com/esprit/microservices/JobApplication.java
@@ -5,9 +5,11 @@ import java.util.stream.Stream;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class JobApplication {
 
 	public static void main(String[] args) {
