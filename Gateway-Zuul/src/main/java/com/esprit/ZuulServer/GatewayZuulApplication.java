5c188036ed3b3a35483c43b4f76b4641c857a378
Oumeima IBNELFEKIH <oumeima.ibnelfekih@esprit.tn>
2020-10-30 14:38:13 +0100

    Zuul Atelier

---
+++ Gateway-Zuul/src/main/java/com/esprit/ZuulServer/GatewayZuulApplication.java
@@ -0,0 +1,19 @@
+package com.esprit.ZuulServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayZuulApplication.class, args);
+	}
+
+}
---
+++ Gateway-Zuul/src/test/java/com/esprit/ZuulServer/GatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.ZuulServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
